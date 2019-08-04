4cc8859fc4f95aedbe30a03b4c792021d8f78ac3
Raphael Stäbler <raphael@productionbuild.de>
2019-08-04 18:58:39 +0200

    Introduced different animations

--- xmas.arduino.ino
+++ xmas.arduino.ino
@@ -19,7 +19,8 @@
 // strandtest example for more information on possible values.
 Adafruit_NeoPixel strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int led[NUMPIXELS];
+unsigned long led[NUMPIXELS];
+int color[NUMPIXELS];
 
 void setup() {
   // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
@@ -30,13 +31,19 @@ void setup() {
   // END of Trinket-specific code.
 
   strip.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.clear();
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(100); // Set BRIGHTNESS to about 1/5 (max = 255)
+
+  //Serial.begin(9600); // Trun on debugging
 }
 
 void loop() {
-  //carousel(20, 6);
-  sparkle(5, 2);
+  unsigned long t = millis();
+  
+  //carousel(80, 6);
+  //lightning(2, 3);
+  sparkle(t, 2, 250);
 }
 
 void carousel(int speed, int length) {
@@ -57,14 +64,14 @@ void carousel(int speed, int length) {
   }
 }
 
-void sparkle(int speed, int duration) {
+void lightning(int speed, int duration) {
   int i = random(0, NUMPIXELS);
 
   led[i] = duration;
 
   for (int l = 0; l < NUMPIXELS; l++) {
     if (led[l] > 0) {
-      strip.setPixelColor(l, strip.ColorHSV(2000, 225, 100));
+      strip.setPixelColor(l, strip.ColorHSV(2000, 225, 255));
       led[l]--;
     }
     else {
@@ -74,4 +81,28 @@ void sparkle(int speed, int duration) {
 
   strip.show();
   delay(speed);
+}
+
+void sparkle(unsigned long t, int speed, int duration) {
+  if (t % speed == 0) {
+    int i = random(0, NUMPIXELS);
+    if (led[i] == 0) {
+      led[i] = t;
+      color[i] = random(2000, 3000);
+    }
+  }
+
+  for (int l = 0; l < NUMPIXELS; l++) {
+    if (led[l] + duration > t) {
+      int d = int(t - led[l]);
+      double m = double(d) / double(duration);
+      strip.setPixelColor(l, strip.ColorHSV(color[l], 225, int(255 * m)));
+    }
+    else {
+      led[l] = 0;
+      strip.setPixelColor(l, strip.Color(0, 0, 0));
+    }
+  }
+  
+  strip.show();
 }
