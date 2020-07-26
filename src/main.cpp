1ebfa79984cb033d0a39681de5c2b977b3e90483
Raphael Stäbler <raphael@productionbuild.de>
2020-07-26 10:55:51 +0200

    Reformat code

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "Adafruit_NeoPixel.h"
 
 #ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
@@ -47,20 +47,21 @@ void halloweenLights(unsigned long t, unsigned long period, unsigned int duratio
 void lightning(unsigned long t, unsigned int speed, unsigned int duration);
 void carousel(unsigned long t, unsigned int speed, unsigned int length);
 
-void setup() {
+void setup()
+{
   // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
   // Any other board, you can remove this part (but no harm leaving it):
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
   clock_prescale_set(clock_div_1);
 #endif
   // END of Trinket-specific code.
-  
+
   Serial.begin(9600); // Trun on debugging
 
   pinMode(BUTTON_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), handleButtonPress, RISING);
 
-  strip.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.begin();            // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.setBrightness(255); // Set BRIGHTNESS
 
   off();
@@ -70,33 +71,48 @@ void setup() {
   Serial.println("All set up");
 }
 
-void loop() {
+void loop()
+{
   unsigned long t = millis();
   unsigned int deltaT = t - prevT;
 
-  if (t < 2000) { return; } // Wait a couple of seconds
+  if (t < 2000)
+  {
+    return;
+  } // Wait a couple of seconds
 
-  while(!strip.canShow());
+  while (!strip.canShow())
+    ;
 
-  switch (mode) {
-    case 1:
-      if (!set) { lightsOn2(); }
-      break;
-    case 2:
-      if (!set) { lightsOn1(); }
-      break;
-    case 3:
-      sparkleXmas(t, deltaT, 5, 300);
-      break;
-    case 4:
-      sparkle(t, deltaT, 5, 300);
-      break;
-    case 5:
-      carousel(t, 80, 6);
-      break;
-    default:
-      if (!set) { off(); }
-      break;
+  switch (mode)
+  {
+  case 1:
+    if (!set)
+    {
+      lightsOn2();
+    }
+    break;
+  case 2:
+    if (!set)
+    {
+      lightsOn1();
+    }
+    break;
+  case 3:
+    sparkleXmas(t, deltaT, 5, 300);
+    break;
+  case 4:
+    sparkle(t, deltaT, 5, 300);
+    break;
+  case 5:
+    carousel(t, 80, 6);
+    break;
+  default:
+    if (!set)
+    {
+      off();
+    }
+    break;
   }
 
   set = true;
@@ -104,42 +120,55 @@ void loop() {
   prevT = t;
 }
 
-void off() {
-  for (int i = 0; i < NUMPIXELS; i++) {
+void off()
+{
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
     strip.setPixelColor(i, strip.Color(0, 0, 0));
   }
-  for (unsigned char i = 0; i < REPEAT; i++) {
+  for (unsigned char i = 0; i < REPEAT; i++)
+  {
     strip.show();
   }
 }
 
-void lightsOn1() {
-  for (int i = 0; i < NUMPIXELS; i++) {
-    if (i % 12 < 4) {
+void lightsOn1()
+{
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    if (i % 12 < 4)
+    {
       strip.setPixelColor(i, strip.Color(0, 0, 0));
     }
-    else {
+    else
+    {
       strip.setPixelColor(i, strip.ColorHSV(6000, 200, 75));
     }
   }
-  
-  for (unsigned char i = 0; i < REPEAT; i++) {
+
+  for (unsigned char i = 0; i < REPEAT; i++)
+  {
     strip.show();
   }
 }
 
-void lightsOn2() {
+void lightsOn2()
+{
   strip.fill(strip.ColorHSV(6000, 200, 50));
-  for (unsigned char i = 0; i < REPEAT; i++) {
+  for (unsigned char i = 0; i < REPEAT; i++)
+  {
     strip.show();
   }
 }
 
-void halloweenLights(unsigned long t, unsigned long period, unsigned int duration) {
-  if (t % period < duration) {
+void halloweenLights(unsigned long t, unsigned long period, unsigned int duration)
+{
+  if (t % period < duration)
+  {
     lightning(t, 2, 3);
   }
-  else {
+  else
+  {
     strip.fill(strip.ColorHSV(1000, 255, 75));
     strip.show();
   }
@@ -147,21 +176,26 @@ void halloweenLights(unsigned long t, unsigned long period, unsigned int duratio
   //lightning(t, 2, 3);
 }
 
-void lightning(unsigned long t, unsigned int speed, unsigned int duration) {
-  if (t % speed != 0) {
+void lightning(unsigned long t, unsigned int speed, unsigned int duration)
+{
+  if (t % speed != 0)
+  {
     return;
   }
-  
+
   int i = random(0, NUMPIXELS);
 
   led[i] = duration;
 
-  for (int l = 0; l < NUMPIXELS; l++) {
-    if (led[l] > 0) {
+  for (int l = 0; l < NUMPIXELS; l++)
+  {
+    if (led[l] > 0)
+    {
       strip.setPixelColor(l, strip.ColorHSV(6000, 200, 255));
       led[l]--;
     }
-    else {
+    else
+    {
       strip.setPixelColor(l, strip.Color(0, 0, 0));
     }
   }
@@ -169,91 +203,114 @@ void lightning(unsigned long t, unsigned int speed, unsigned int duration) {
   strip.show();
 }
 
-void sparkle(unsigned long t, unsigned int deltaT, unsigned int speed, unsigned int duration) {
-  if (t % speed == 0) {
+void sparkle(unsigned long t, unsigned int deltaT, unsigned int speed, unsigned int duration)
+{
+  if (t % speed == 0)
+  {
     int i = random(0, NUMPIXELS);
-    if (led[i] == 0) {
+    if (led[i] == 0)
+    {
       led[i] = duration;
     }
   }
 
-  for (int l = 0; l < NUMPIXELS; l++) {
-    if (led[l] > deltaT) {
+  for (int l = 0; l < NUMPIXELS; l++)
+  {
+    if (led[l] > deltaT)
+    {
       led[l] -= deltaT;
       double d = double(led[l]) / double(duration);
       double m = -d * (d - 2.0D); // Ease-out
       strip.setPixelColor(l, strip.ColorHSV(5000, 235, int(255.0D * m)));
     }
-    else {
+    else
+    {
       led[l] = 0;
       strip.setPixelColor(l, strip.Color(0, 0, 0));
     }
   }
-  
-  for (unsigned char i = 0; i < REPEAT; i++) {
+
+  for (unsigned char i = 0; i < REPEAT; i++)
+  {
     strip.show();
   }
 }
 
-void sparkleXmas(unsigned long t, unsigned int deltaT, unsigned int speed, unsigned int duration) {
-  if (t % speed == 0) {
+void sparkleXmas(unsigned long t, unsigned int deltaT, unsigned int speed, unsigned int duration)
+{
+  if (t % speed == 0)
+  {
     int i = random(0, NUMPIXELS);
-    if (led[i] == 0) {
+    if (led[i] == 0)
+    {
       led[i] = duration;
       color[i] = deltaT % 2 == 0;
     }
   }
 
-  for (int l = 0; l < NUMPIXELS; l++) {
-    if (led[l] > deltaT) {
+  for (int l = 0; l < NUMPIXELS; l++)
+  {
+    if (led[l] > deltaT)
+    {
       led[l] -= deltaT;
       double d = double(led[l]) / double(duration);
       double m = -d * (d - 2.0D); // Ease-out
-      if (color[l]) {
+      if (color[l])
+      {
         strip.setPixelColor(l, strip.ColorHSV(21845, 255, int(200.0D * m)));
       }
-      else {
+      else
+      {
         strip.setPixelColor(l, strip.ColorHSV(0, 255, int(255.0D * m)));
       }
     }
-    else {
+    else
+    {
       led[l] = 0;
       strip.setPixelColor(l, strip.Color(0, 0, 0));
     }
   }
-  
-  for (unsigned char i = 0; i < REPEAT; i++) {
+
+  for (unsigned char i = 0; i < REPEAT; i++)
+  {
     strip.show();
   }
 }
 
-void carousel(unsigned long t, unsigned int speed, unsigned int length) {
-  if (t % speed != 0) {
+void carousel(unsigned long t, unsigned int speed, unsigned int length)
+{
+  if (t % speed != 0)
+  {
     return;
   }
 
   unsigned int s = currentLED;
 
-  for (unsigned int i = 0; i < NUMPIXELS; i++) {
+  for (unsigned int i = 0; i < NUMPIXELS; i++)
+  {
     int v = abs(i - s);
 
-    if (v % (length * 2) < length) {
+    if (v % (length * 2) < length)
+    {
       strip.setPixelColor(i, strip.ColorHSV(250, 255, 100));
     }
-    else {
+    else
+    {
       strip.setPixelColor(i, strip.ColorHSV(2000, 225, 100));
     }
   }
-  
+
   strip.show();
-  
+
   currentLED = (currentLED + 1) % NUMPIXELS;
 }
 
-void handleButtonPress() {
+void handleButtonPress()
+{
   unsigned long t = millis();
 
-  if (t > buttonLastRead + buttonDebounceDelay) {
+  if (t > buttonLastRead + buttonDebounceDelay)
+  {
     mode = getMode(mode + 1);
     buttonLastRead = t;
 
@@ -265,6 +322,7 @@ void handleButtonPress() {
   }
 }
 
-unsigned int getMode(unsigned int m) {
+unsigned int getMode(unsigned int m)
+{
   return m % 7;
 }
